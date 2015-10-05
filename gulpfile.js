d5dce9f8482867d18a6e43e97d91bbbbb99b0a19
SuThaw <suthaw524@gmail.com>
2015-10-05 10:53:15 +0630

    add jade render and copy index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,41 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	 del = require('del'),
+	 jade = require('gulp-jade'),
+	 webserver = require('gulp-webserver');
+
+
+
+var paths = {
+	temp:'public',
+	index:'app/index.jade'
+};
+
+// to copy all the files
+gulp.task('copyAll',function(){
+	return gulp
+				.src(paths.index)
+				.pipe(jade())
+				.pipe(gulp.dest(paths.temp));
+});
+
+gulp.task('serve',function(){
+	return gulp
+				.src(paths.temp)
+				.pipe(webserver({
+					livereload:true,
+					
+				}));
+});
+
+gulp.task('clean',function(){
+	del([paths.temp]);
+
+});
+
+gulp.task('watch',['serve'],function(){
+	gulp.watch(paths.index,['copyAll']);
+})
+gulp.task('default',['watch']);
+
+
+
